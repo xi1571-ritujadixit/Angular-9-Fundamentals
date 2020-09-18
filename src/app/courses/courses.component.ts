ccdbf9e48e7f10dbcfdb8b7cb527995565520154
Rituja <rituja.dixit@xebia.com>
2020-09-18 15:36:10 +0530

    Update: Form added

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -6,19 +7,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
-  courses = [
-    {
-      id: 1,
-      title: 'Angular 9 Fundamentals',
-      description: 'Learn the fundamentals of Angular 9',
-      percentComplete: 26,
-      favorite: true
+    selectedCourse = null;
+    courses = [
+        {
+            id: 1,
+            title: 'Angular 9 Fundamentals',
+            description: 'Learn the fundamentals of Angular 9',
+            percentComplete: 26,
+            favorite: true
+        },
+        {
+            id: 2,
+            title: 'JavaScript The Really REALLY HARD PARTS',
+            description: 'Worship Will Sentence',
+            percentComplete: 50,
+            favorite: true
+        }
+    ];
+
+    constructor() { }
+
+    ngOnInit(): void {
+        this.resetSelectedCourse();
     }
-  ];
 
-  constructor() { }
+    resetSelectedCourse() {
+        const emptyCourse = {
+            id: null,
+            title: '',
+            description: '',
+            percentComplete: 0,
+            favorite: false
+        };
 
-  ngOnInit(): void {
-  }
+        this.selectedCourse = emptyCourse;
+    }
+
+    selectCourse(course) {
+        this.selectedCourse = course;
+    }
+
+    saveCourse() {
+        console.log('SAVE COURSE!');
+    }
+
+    deleteCourse(courseId) {
+        console.log('COURSE DELETED!', courseId);
+    }
+
+    cancel() {
+        this.resetSelectedCourse();
+    }
 
 }
